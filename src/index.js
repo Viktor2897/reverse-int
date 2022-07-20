e6d76764fa609b1ad603bd3cd9629f7a627cca28
Viktor Manko <mankovictor@gmail.com>
2022-07-20 07:12:28 +0300

    reverse int is done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    let nAsString = String(n);
+    let i = 0;
+    let result = '';
+    for (i; i < nAsString.length; i++) {
+        result = nAsString[i] + result;
+    }
+    if (result[result.length - 1] === '-') {
+        result = result.slice(0, -1);
+    }
+    return result;
 }
