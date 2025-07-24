8d3d0832de02d3f8fd08bdb3719bdd38192f0bb3
Viktor2897 <mankovictor@gmail.com>
2025-07-24 12:32:58 +0300

    try

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  const nAsString = String(n);
+  let i = 0;
+  let result = '';
+  for (i; i < nAsString.length; i += 1) {
+    result = nAsString[i] + result;
+  }
+  if (result[result.length - 1] === '-') {
+    result = result.slice(0, -1);
+  }
+  return result;
 };
